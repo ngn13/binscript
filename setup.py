13c5aad343ed2f2a5602ae0aae40832964926750
ngn <ngn13proton@proton.me>
2023-03-04 16:53:22 +0300

    also fixed the version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1" 
-DESCRIPTION = "A simple scriptting language for buffer overflow exploitation"
+VERSION = "0.0.2" 
+DESCRIPTION = "c"
 
 setup(
         name="binscript", 
